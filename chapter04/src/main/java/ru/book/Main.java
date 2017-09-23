2243c24bf51ba2282ea36e9748d612b1cc5c364a
Alexey Elnikov <frey87@yandex.ru>
2017-09-23 13:22:19 +0300

    add chapter04

--- chapter04/src/main/java/ru/book/Main.java
+++ chapter04/src/main/java/ru/book/Main.java
@@ -7,14 +7,17 @@ import ru.book.chapter04.PerfomanceException;
 import ru.book.chapter04.Performer;
 
 public class Main {
-    public static void main(String[] args) throws PerfomanceException {
+    public static void main(String[] args) throws PerfomanceException, ru.book.javaconfig.PerfomanceException {
 
         ApplicationContext ctx = new ClassPathXmlApplicationContext
                 ("META-INF/spring/spring-idol.xml");
 
-        Performer kenny = (Performer) ctx.getBean("kenny");
-        kenny.perform();
+//        Performer kenny = (Performer) ctx.getBean("kenny");
+//        kenny.perform();
+//        System.out.println("---------------------");
 
 
+        ru.book.javaconfig.Performer alex = (ru.book.javaconfig.Performer) ctx.getBean("duke15");
+        alex.perform();
     }
 }
